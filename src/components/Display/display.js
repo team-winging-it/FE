10ad620a2a738c53955fff1fb6bafb57b9047838
Chris-Delfaus <ChrisDelf@protonmail.com>
2020-04-29 12:10:41 -0400

    able to get the user id from  the backend now

--- src/components/Display/display.js
+++ src/components/Display/display.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { generateMap, getMap } from '../../actions/charActions';
+import { generateMap, getMap} from '../../actions/charActions';
+import {getUserInfo} from '../../actions/authActions'
 import axios from 'axios';
 
-const Display = props => {
+const Display = (props) => {
   console.log(props);
   const token = localStorage.getItem('token');
-  //
+  console.log(props.playerMap)
+  useEffect(() => {
+    props.getUserInfo(props.username)
+
+  }, [])
+
   // return (
   //   <button
   //     onClick={() =>
@@ -32,20 +38,21 @@ const Display = props => {
   //   </button>
   // );
 
-  return(
+  return (
     <>
-    <button onClick ={ ()=> props.generateMap()}>Generate</button>
-    <button onClick ={ ()=> props.getMap()}>GetMap</button>
+      <button onClick={() => props.generateMap()}>Generate</button>
+      <button onClick={() => props.getMap()}>GetMap</button>
     </>
   );
 };
 const mapStateToProps = state => {
   return {
-    maps: state.charReducer.playerMap
-  }
-}
+    maps: state.charReducer.playerMap,
+    username: state.authReducer.user,
+  };
+};
 
 export default connect(
   mapStateToProps,
-  { generateMap, getMap }
+  { generateMap, getMap, getUserInfo}
 )(Display);
