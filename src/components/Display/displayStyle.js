27043596d7f986583c184d1eb0e09d725eb25343
Chris-Delfaus <ChrisDelf@protonmail.com>
2020-05-04 18:21:33 -0400

    creating the ground work for css for the display page

---
+++ src/components/Display/displayStyle.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  justify-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #0a0;
+  height: 100vh;
+  .buttonContainer {
+    justify-content: center;
+  }
+`;
--- src/components/auth/authStyle.js
+++ src/components/auth/authStyle.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
   justify-items: center;
   display: flex;
   justify-content: center;
-  background: #282e74;
+  background: #0a0;
   .map {
     width: 60%;
     @media (max-width: 1024px) {
